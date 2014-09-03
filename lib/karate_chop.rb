4ee8752fed2d61e3ae293b6cfa78f48de04fa985
david.lawson <david.lawson@corelogic.co.uk>
2014-09-03 20:11:23 +0100

    karate kata - tidyup

--- lib/karate_chop.rb
+++ lib/karate_chop.rb
@@ -4,17 +4,16 @@ class KarateChop
   end
   
   def inner_chop(offset, int, array_of_ints)
-	return -1 if(array_of_ints.empty?)
+    return -1 if(array_of_ints.empty?)
     ints_size = array_of_ints.size
-	half_way_index = ints_size/2
-	
-	if(array_of_ints[half_way_index] == int)
+    half_way_index = ints_size/2
+
+    if(array_of_ints[half_way_index] == int)
 	  return half_way_index + offset
-	elsif(int < array_of_ints[half_way_index])
-	  return inner_chop(offset, int, array_of_ints[0, half_way_index])
+    elsif(int < array_of_ints[half_way_index])
+      return inner_chop(offset, int, array_of_ints[0, half_way_index])
     else
-	  return inner_chop(offset + half_way_index + 1, int, array_of_ints[half_way_index + 1, ints_size])	
-	end  
+      return inner_chop(offset + half_way_index + 1, int, array_of_ints[half_way_index + 1, ints_size])	
+    end  
   end 
-  
 end
